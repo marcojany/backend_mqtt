bea1b25a0a3364907bc06cfadcee791647c75ebe
marcojany <194432109+marcojany@users.noreply.github.com>
2025-09-05 22:35:55 +0200

    add cors middleware to enable cross-origin requests

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mqtt from "mqtt";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // --- Connessione MQTT ---
 const options = {
